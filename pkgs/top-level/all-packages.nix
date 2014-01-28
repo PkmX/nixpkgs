98a434e7dc612f2b46a881825be68c8b5837c10a
James Cook <james.cook@utoronto.ca>
2014-01-28 02:26:09 -0800

    Re-organized the cryptocurrency code.
    Each cryptocurrency in a separate file, and less duplication of code
    between the headless and gui expressions.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7445,11 +7445,15 @@ let
 
   bibletime = callPackage ../applications/misc/bibletime { };
 
-  bitcoinWallets = recurseIntoAttrs (
-    callPackage ../applications/misc/bitcoin { }
-  );
+  build_bitcoin = callPackage ../applications/misc/bitcoin/build_bitcoin.nix { };
 
-  bitcoin = bitcoinWallets.bitcoin-qt;
+  bitcoin = callPackage ../applications/misc/bitcoin/bitcoin.nix { };
+
+  litecoin = callPackage ../applications/misc/bitcoin/litecoin.nix { };
+
+  namecoin = callPackage ../applications/misc/bitcoin/namecoin.nix { };
+
+  ppcoin = callPackage ../applications/misc/bitcoin/ppcoin.nix { };
 
   bitlbee = callPackage ../applications/networking/instant-messengers/bitlbee {
     # For some reason, TLS support is broken when using GnuTLS 3.0 (can't
